987a277af8e6d8ab4e4509248a9ede4dfbf6e379
wmrwmr <wnrak0116@gmail.com>
2021-10-15 11:04:01 +0900

    day1015

--- java_project/FirstJava/src/chapter06/Exam.java
+++ java_project/FirstJava/src/chapter06/Exam.java
@@ -46,25 +46,25 @@ public class Exam {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		int index = (int) (Math.random() * 10) + 1;
-
-		int[] arr1 = new int[index];
-
-		System.out.println(index + "개의 숫자를 입력해주세요");
-		for (int i = 0; i < index; i++) {
-			arr1[i] = scanner.nextInt();
-		}
-
-		System.out.println("======= 배열 =======");
-		for (int i = 0; i < index; i++) {
-			System.out.println(arr1[i]);
-		}
-
-		System.out.println("======= 최소값 =======");
-		System.out.println(Exam.miniValue(arr1));
-
-		System.out.println("======= 최대값 =======");
-		System.out.println(Exam.maxValue(arr1));
+//		int index = (int) (Math.random() * 10) + 1;
+//
+//		int[] arr1 = new int[index];
+//
+//		System.out.println(index + "개의 숫자를 입력해주세요");
+//		for (int i = 0; i < index; i++) {
+//			arr1[i] = scanner.nextInt();
+//		}
+//
+//		System.out.println("======= 배열 =======");
+//		for (int i = 0; i < index; i++) {
+//			System.out.println(arr1[i]);
+//		}
+//
+//		System.out.println("======= 최소값 =======");
+//		System.out.println(Exam.miniValue(arr1));
+//
+//		System.out.println("======= 최대값 =======");
+//		System.out.println(Exam.maxValue(arr1));
 
 		// 2차원 배열의 선언
 		int[][] arr3 = { 
@@ -79,40 +79,97 @@ public class Exam {
 	}
 
 	public static void addOneDArr(int[][] arr, int add) {
-
-		// 2차원 배열의 출력
-		// 행
-		for (int i = 0; i < arr.length; i++) {
-
-			// 열
-			for (int j = 0; j < arr[i].length; j++) {
-				System.out.print(arr[i][j] + "\t");
+		
+		
+		int [][] arr1 = {
+				{1,2,3},
+				{4,5,6},
+				{7,8,9},
+				{10,11,12}
+		};
+		
+		// 출력
+		for(int i = 0; i<arr1.length; i++) {
+			for (int j = 0; j<arr1[i].length; j++) {
+				System.out.print(arr1[i][j] + "\t");
 			}
 			System.out.println();
 		}
 		
 		
-		// 매개 변수로 전달받은 숫자를 각 요소에 더해준다.
-		for (int i = 0; i < arr.length; i++) {
-
-			for (int j = 0; j < arr[i].length; j++) {
-				arr[i][j] += add;
+		// 임시 배열 선언
+		int[] temp = arr1[arr1.length-1];
+		
+		for (int i = arr1.length-1; i>0; i--) {
+			
+			arr1[i] = arr1[i-1];
+		}
+		
+		arr1[0] = temp;
+		
+		System.out.println();
+		
+		for(int i = 0; i<arr1.length; i++) {
+			for (int j = 0; j<arr1[i].length; j++) {
+				System.out.print(arr1[i][j] + "\t");
 			}
 			System.out.println();
 		}
 		
-		System.out.println("각 요소에 숫자를 더하기 ===================");
 		
-		for (int i = 0; i < arr.length; i++) {
 
-			for (int j = 0; j < arr[i].length; j++) {
-				System.out.print(arr[i][j] + "\t");
-			}
-			System.out.println();
-		}
+//		// 2차원 배열의 출력
+//		// 행
+//		for (int i = 0; i < arr.length; i++) {
+//
+//			// 열
+//			for (int j = 0; j < arr[i].length; j++) {
+//				System.out.print(arr[i][j] + "\t");
+//			}
+//			System.out.println();
+//		}
+//		
+//		
+//		// 매개 변수로 전달받은 숫자를 각 요소에 더해준다.
+//		for (int i = 0; i < arr.length; i++) {
+//
+//			for (int j = 0; j < arr[i].length; j++) {
+//				arr[i][j] += add;
+//			}
+//			System.out.println();
+//		}
+//		
+//		System.out.println("각 요소에 숫자를 더하기 ===================");
+//		
+//		for (int i = 0; i < arr.length; i++) {
+//
+//			for (int j = 0; j < arr[i].length; j++) {
+//				System.out.print(arr[i][j] + "\t");
+//			}
+//			System.out.println();
+//		}
 		
 
 	}
+	
+//	문제 3.
+//	다음의 형태로 표현된 2차원 배열이 존재한다고 가정해 보자.
+//	1 2 3
+//	4 5 6
+//	7 8 9
+//	이러한 형태를 갖는 int형 2차원 배열이 인자로 전달되면, 다음의 형태로 배열
+//	의 구조를 변경시키는 메서드를 정의해 보자.
+//	7 8 9
+//	1 2 3
+//	4 5 6
+//	즉 총 N행으로 이뤄진 2차원 배열이 존재한다면, 메서드 호출 시, 1행은 2행
+//	으로 이동이 이뤄져야한다. 이번에도 마찬가지로 배열의 가로, 세로길이에 상
+//	관 없이 동작을 하도록 메서드가 정의되어야 하며, 정의된 메서드의 확인을
+//	위한 main메서드도 함께 정의하자.
+	
+	
+	
+	
 
 
 
