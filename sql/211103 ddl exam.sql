69c2437b26319d686967f8f67b92b6234d56f024
wmrwmr <wnrak0116@gmail.com>
2021-11-04 11:44:51 +0900

    day1104

--- sql/211103 ddl exam.sql
+++ sql/211103 ddl exam.sql
@@ -9,20 +9,23 @@ create table phoneInfo_basic (
 
 
 create table phoneInfo_univ (
-    idx number(6) not null,
-    fr_u_major varchar2(20) default 'N',
-    fr_u_year number(1) default '1',
+    idx number(6),
+    fr_u_major varchar2(20) default 'N' not null,
+    fr_u_year number(1) default '1' not null,
     fr_ref number(7) not null,
-    constraint phoneInfo_univ_fr_u_year_ck check(fr_u_year between 0 and 5),
+    constraint phoneInfo_univ_fr_u_year_ck check(fr_u_year between 1 and 4),
     constraint phoneInfo_univ_idx_pk primary key (idx),
     constraint phoneInfo_univ_fr_ref_fk foreign key (fr_ref) references phoneInfo_basic (idx)
 );
 
 create table phoneInfo_com (
     idx number(6),
-    fr_c_company varchar2(20) default 'N',
+    fr_c_company varchar2(20) default 'N' not null,
     fr_ref number(6) not null,
     constraint phoneInfo_com_idx_pk primary key (idx),
     constraint phoneInfo_com_fr_ref_fk foreign key (fr_ref) references phoneInfo_basic (idx)
 );
+
+
+
 
